05312660751b262f64cfaab7a7d71f03454cfca2
Jeenson Aguilar <jaguilar@optimeconsulting.com>
2022-11-07 19:13:43 -0500

    back to home

--- src/App.js
+++ src/App.js
@@ -1,21 +1,13 @@
 import React from 'react';
 
 import './App.css';
-import '@aws-amplify/ui-react/styles.css';
-import { Authenticator } from '@aws-amplify/ui-react';
+
 
 
 function App() {
   return (
     <div className="App">
-      <Authenticator>
-        {({ signOut, user }) => (
-          <main>
-            <h1>Hello {user.username}</h1>
-            <button onClick={signOut}>Sign out</button>
-          </main>
-        )}
-      </Authenticator>
+      <h1>Hola Jeenson Aguilar Correa</h1>
     </div>
   );
 }
